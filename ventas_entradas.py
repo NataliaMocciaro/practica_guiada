8b0da7a09519451fdb5442944f7f951b9f2e36f6
Natalia Mocciaro <natalia.mocciaro@ispi4038.edu.ar>
2023-07-03 18:00:13 -0300

    Inicio proyecto, estructura básica

---
+++ ventas_entradas.py
@@ -0,0 +1,75 @@
+
+
+#declaración de variables - valores
+
+entradas_populares = 100
+entradas_exclusivas = 50
+entradas_vip = 20
+
+valor_populares = 1000
+valor_exclusivas = 2000
+valor_vip = 3000
+
+total_recaudado = 0
+total_clientes = 0
+
+def calculos_para_entradas_vip(cant):
+#asignado a Omar
+    pass
+
+def calculos_para_entradas_exclusivas(cant):
+    #asignado a Damian
+    pass
+
+def calculos_para_entradas_populares(cant):
+    #asignado a Agustín
+    pass
+
+while True:
+
+    #Ingreso y fin del sistema
+    print("""Ingrese:
+1- Para realizar una venta
+2- Para finalizar la venta""")
+    venta = int(input())
+
+    if venta == 2:
+        print("Recaudación total: ", total_recaudado)
+        print("Clientes atendidos: ", total_clientes)
+        print("\nSobrantes: ")
+        print("Entradas Vip: ", entradas_vip)
+        print("Entradas Exclusivas: ", entradas_exclusivas)
+        print("Entradas Populares: ", entradas_populares)
+        print("\nGracias por utilizar el sistema")
+        exit()
+    
+    elif venta == 1:
+
+        total_clientes = total_clientes + 1
+        while True:
+
+            # cada compra
+            print(
+"""Tipo de entradas:
+1- Entradas populares
+2- Entradas exclusivas
+3- Entradas vip
+4- Finalizar Compra""")
+            compra = int(input())
+        
+            if compra == 4:
+                break
+            elif compra == 3:
+                cantidad_solicitada = int(input("Cuantas entradas necesita: "))
+                calculos_para_entradas_vip(cantidad_solicitada)
+            elif compra == 2:
+                cantidad_solicitada = int(input("Cuantas entradas necesita: "))
+                calculos_para_entradas_exclusivas(cantidad_solicitada)
+            elif compra == 1:
+                cantidad_solicitada = int(input("Cuantas entradas necesita: "))
+                calculos_para_entradas_populares(cantidad_solicitada)
+            else:
+                print("Debe esegir una opción correcta")  
+
+    else:
+        print("Debe esegir una opción correcta")
